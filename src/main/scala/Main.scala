cf73ba4d96ff01ed66bf2158d70cd8f793877e59
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-06-19 10:56:48 +0300

    removed commented-out files

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,6 @@
 package x
 
-//import scala.concurrent.{Future}
-//import cps.async
-//import cps.CpsMonad
-//import cps.CpsMonadInstanceContext
-//import scala.concurrent.ExecutionContext
-//import scala.concurrent.ExecutionContext.Implicits.global
-
-
-//given CpsMonad[Future] with CpsMonadInstanceContext[Future] with
-//  def pure[A](a:A):Future[A] = ???
-//  def map[A,B](fa:Future[A])(f:A=>B):Future[B] = ???
-//  def flatMap[A,B](fa:Future[A])(f:A=>Future[B]):Future[B] = ???
-
-//given printCode: cps.macros.flags.PrintCode.type = cps.macros.flags.PrintCode;
-//given printTree: cps.macros.flags.PrintTree.type = cps.macros.flags.PrintTree;
-
-//def hello: Future[?] = {
 def hello = {
-  //async1[Future] {
   xtransform {
     val a: Seq[Generic[?]] = null
     a
